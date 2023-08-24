2985e8ec4304c2debd5157eb094bae254808d38c
wavedoppp <21032003k@gmail.com>
2023-08-24 15:16:30 +0300

    Добавьте файлы проекта.

---
+++ APP1.1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace APP1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Добро пожаловать. Введите свои Имя и фамилию: ");
+            string fullName = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {fullName}!");
+            Console.WriteLine($"Введите свою дату рождения!");
+            Console.WriteLine();
+            Console.WriteLine($"День:");
+            int day = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Месяц:");
+            int month = int.Parse(Console.ReadLine());
+
+            Console.Write($"{fullName} ");
+            Console.WriteLine() ;
+            int min = month - day ;
+            int sum = month + day;
+            int del = month % day;
+            int um = month * day;
+            Console.Write($"Разница: {min}");
+            Console.WriteLine() ;
+            Console.Write($"Сумма: {sum}");
+            Console.WriteLine();
+            Console.Write($"Остаток: {del}");
+            Console.WriteLine();
+            Console.Write($"Умножено: {um}");
+            Console.WriteLine();
+
+
+
+
+            Console.ReadLine() ;
+        }
+    }
+}
